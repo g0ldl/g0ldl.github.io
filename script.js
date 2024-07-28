9fe15aaf1019e2fbb4e6a84455f6f63102c8bbf8
Goldi <102404584+g0ldl@users.noreply.github.com>
2024-07-28 19:37:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ document.addEventListener('click', e => {
     }, 500);
 });
 
+//for contact page
+gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 20%',
+      scrub: true,
+    },
+  });
+});
