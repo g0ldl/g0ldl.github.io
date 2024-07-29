45417876d1ab37dedb308998cfda25a9d41ae7dc
Goldi <102404584+g0ldl@users.noreply.github.com>
2024-07-29 18:10:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-
-//custom cursor by Rahul Chaudhary on CodePen <3
-const cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
-});
-
-document.addEventListener('click', e => {
-    cursor.classList.add("expand");
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500);
-});
-
-//for contact page
-gsap.registerPlugin(ScrollTrigger);
-
-const textElements = gsap.utils.toArray('.text');
-
-textElements.forEach(text => {
-  gsap.to(text, {
-    backgroundSize: '100%',
-    ease: 'none',
-    scrollTrigger: {
-      trigger: text,
-      start: 'center 80%',
-      end: 'center 20%',
-      scrub: true,
-    },
-  });
-});
-
 // LOADING SCREEN
 
 const progressBar = document.getElementById('progress-bar');
@@ -67,5 +34,38 @@ document.addEventListener('DOMContentLoaded', () => {
     progress += increment;
   }, 100);
 });
+
+//custom cursor by Rahul Chaudhary on CodePen <3
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
+
+//for contact page
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 20%',
+      scrub: true,
+    },
+  });
+});
+
+
 
 
