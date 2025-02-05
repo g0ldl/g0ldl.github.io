684bd87d01a2118b702b312b41a9fdcb206aa666
Goldium Wu <102404584+g0ldl@users.noreply.github.com>
2025-02-06 05:19:35 +0700

    Add files via upload
    -New welcome page
    -Rewrote about me paragraph
    -project gallery bug fix

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// LOADING SCREEN (Thanks to @juxtopposed) <3
-
+//Loading screen inspired by @juxtopposed
 const progressBar = document.getElementById('progress-bar');
 const counter = document.getElementById('progress-counter');
 const loadingScreen = document.getElementById('loading-screen');
@@ -35,11 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 100);
 });
 
-//custom cursor by Rahul Chaudhary on CodePen <3
+//Custom cursor inspired by Rahul Chaudhary
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+    cursor.setAttribute("style", "top: " + (e.pageY - 5) + "px; left: " + (e.pageX - 5) + "px;")
 });
 
 document.addEventListener('click', e => {
@@ -49,7 +48,7 @@ document.addEventListener('click', e => {
     }, 500);
 });
 
-//for contact page
+//Contact page
 const textElements = gsap.utils.toArray('.text');
 
 textElements.forEach(text => {
@@ -65,6 +64,90 @@ textElements.forEach(text => {
   });
 });
 
+//Welcome morph
+const elts = {
+  text1: document.getElementById("morph1"),
+  text2: document.getElementById("morph2")
+};
 
+const texts = [
+  "",
+  "",
+  "Welcome",
+  "to my",
+  "website",
+  "I hope",
+  "my curation",
+  "is pleasing",
+  "to you."
+];
 
+const morphTime = 1;
+const cooldownTime = 0.25;
 
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+      cooldown = cooldownTime;
+      fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+      if (shouldIncrementIndex) {
+          textIndex++;
+      }
+
+      doMorph();
+  } else {
+      doCooldown();
+  }
+}
+
+animate();
